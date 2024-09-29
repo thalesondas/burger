94db5cb11a5856643b6da34b4cab5fc6ff6a904a
Thales <thalesondas0@gmail.com>
2024-09-29 13:28:21 -0300

    Criando o Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { Alert, Container, Image } from "react-bootstrap";
+import Logo from '../images/burger_logo.jpg'
+import '../assets/Header.css'
+
+const Header = () => {
+    return(
+        <header fluid className="header-background d-flex flex-column justify-content-center align-items-center">
+            <Image src={Logo} alt="Rock'n'roll Burger logo" className="burger-logo" roundedCircle />
+            <h1 className="mt-4 fw-bold">Rock 'n' Roll Burger</h1>
+            <h2 className="mt-2 fs-3">Aqui, o sabor é o verdadeiro rockstar!</h2>
+            <span className="d-block fs-6 mt-4">Rua das Avenidas 123, Jardim Headbanger, Americana - SP</span>
+            <Alert variant="danger" className="d-block fs-6 mt-4">Terça a Domingo - 17:00 às 23:00</Alert>
+        </header>
+    )
+}
+
+export default Header;
