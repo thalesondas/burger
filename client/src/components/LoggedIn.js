9c03eb116bc74f6ff05e633177f537920d1ef3e6
Thales <thalesondas0@gmail.com>
2024-10-06 18:49:54 -0300

    Saudação diferente para cada hora do dia

--- client/src/components/LoggedIn.js
+++ client/src/components/LoggedIn.js
@@ -2,20 +2,35 @@ import { Button, Container } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { logout } from "../redux/authSlice";
 import '../assets/LoggedBox.css'
+import { useEffect, useState } from "react";
 
 const LoggedIn = () => {
 
+    useEffect(() => {
+        const hours = new Date().getHours();
+        
+        if(hours <= 11){
+            setGreetings("Bom dia");
+        } else if(hours >= 18){
+            setGreetings("Boa noite");
+        } else {
+            setGreetings("Boa tarde");
+        }
+    }, [])
+
+    const [greetings, setGreetings] = useState("")
+
     const dispatch = useDispatch();
 
-    const handleLougOut = () => {
+    const handleLogOut = () => {
         localStorage.removeItem('token');
         dispatch(logout());
     }
 
     return(
         <Container className="d-flex justify-content-center align-items-center">
-            <span>Olá, XXX</span>
-            <Button onClick={handleLougOut} className="btn-footer ms-3 py-1 px-2">Sair da conta</Button>
+            <span className="fw-bold">{greetings}, XXX</span>
+            <Button onClick={handleLogOut} className="btn-footer ms-3 py-1 px-2">Sair da conta</Button>
         </Container>
     )
 }
--- client/src/components/NotLoggedIn.js
+++ client/src/components/NotLoggedIn.js
@@ -6,13 +6,13 @@ const NotLoggedIn = () => {
     return(
         <Row className="ps-4 w-100 d-flex justify-content-around">
             <Col className="col-auto d-flex align-items-center">
-                <span>Você não está conectado</span>
+                <span className="fw-bold">Você não está conectado</span>
                 <Link to='/login'>
                     <Button className="btn-footer ms-2 py-1 px-2">Entrar</Button>
                 </Link>
             </Col>
             <Col className="col-auto d-flex flex-row align-items-center">
-                <span>Não tem uma conta?</span>
+                <span className="fw-bold">Não tem uma conta?</span>
                 <Link to='/register'>
                     <Button className="btn-footer ms-2 py-1 px-2">Criar Conta</Button>
                 </Link>
