d5d4d2f94cbee4becc802efedf755dc4b6d930d3
Thales <thalesondas0@gmail.com>
2024-10-07 11:45:17 -0300

    Melhorando o Card de lanches em telas menores

--- client/src/components/Burger.js
+++ client/src/components/Burger.js
@@ -1,18 +1,17 @@
 import { Card, Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { addCartItem } from "../redux/cartSlice";
-import '../assets/Burger.css'
+import '../assets/Burger.css';
 
 const Burger = ({ burger }) => {
-
     const dispatch = useDispatch();
 
     return (
         <Card md={6} className="bg-burger mb-3">
-            <div className="d-flex">
+            <div className="d-flex flex-column flex-sm-row align-items-sm-start align-items-center">
                 <Card.Img src={burger.imageUrl} alt={burger.name} className="img-burger" rounded />
                 <Card.Body className="ms-2 me-1">
-                    <Card.Title className="custom-font mb-3 fs-4">{burger.name}</Card.Title>
+                    <Card.Title className="custom-font mb-3 fs-4 text-center text-sm-start">{burger.name}</Card.Title>
                     <Card.Text>{burger.description}</Card.Text>
                     <div className="d-flex justify-content-between align-items-center">
                         <span className="fw-bold fs-5">R$ {burger.price.toFixed(2).replace('.', ',')}</span>
